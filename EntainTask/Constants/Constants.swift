fa1e7900f812558403dacefea58d2aea2ed0b447
Prajoth Antonio <dsaprajoth@gmail.com>
2024-12-14 01:32:48 +1100

    Folder Structure

--- EntainTask/Constants/Constants.swift
+++ EntainTask/Constants/Constants.swift
@@ -12,6 +12,12 @@ struct APIConstants {
     static let endpoint = "https://api.neds.com.au/rest/v1/racing/?method=nextraces&count=5"
 }
 
+struct CategoryIds {
+    static let horseRacing = "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+    static let harnessRacing = "161d9be2-e909-4326-8c2c-35ed71fb460b"
+    static let greyHoundRacing = "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+}
+
 struct ColorConstants {
     static let themeLight = Color("ThemeOrangeLight")
     static let chipSelected = Color("chipSelected")
