8f8ffd97f743b6b9a80f41b3fc0d855479e79508
Prajoth Antonio <dsaprajoth@gmail.com>
2024-12-13 23:47:18 +1100

    Error handling

--- EntainTask/Constants.swift
+++ EntainTask/Constants.swift
@@ -16,6 +16,11 @@ struct ColorConstants {
     static let themeLight = Color("ThemeOrangeLight")
     static let chipSelected = Color("chipSelected")
     static let chipUnselected = Color("chipUnselected")
+    static let chipIconSelected = Color("chipIconSelected")
+    static let chipIconUnselected = Color("chipIconUnselected")
+    static let chipBgSelected = Color("chipBgSelected")
+    static let chipBgUnselected = Color("chipBgUnselected")
+    static let raceTileIcon = Color("raceTileIcon")
 }
 
 struct AssetConstants {
